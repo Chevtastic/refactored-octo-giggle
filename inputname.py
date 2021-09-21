4700c501cb9c006520f2df1c2d3a790940edaf82
chevtastic <christopherchevrier@gmail.com>
2021-09-20 23:53:35 -0400

    updating with latest code

--- inputname.py
+++ inputname.py
@@ -1,25 +1,33 @@
-import time
 
+print("Greetings! What's your first name")
 
-print('Greetings! Whats your first name')
+firstName = input() # get user input for first name
+fNameLength = len(firstName) #get length of first name
+capitalFirst = firstName.capitalize() #capitalize first name
 
-firstName = input()
-fNameLength = len(firstName)
+print("okay so your first name is " + capitalFirst + "\nhere's a fun fact, your first name is " + str(fNameLength) + " characters long")
 
-
-print("okay so your first name is "+ firstName + " and, fun fact, your first name is " + str(fNameLength) + " characters long")
-
+#While loop to spell out each letter 
 i=0
 while i < len(firstName):
-    print(firstName[i], end=' ')
+    print(capitalFirst[i])#, end=' ')
     i += 1
 
-#time.sleep(3)
 
-#print(f"{firstName}{fNameLength}")
+# trying out f strings but commenting out for this project
+# print(f"{firstName} {fNameLength}")
 
-# print(firstName + " what is your LAST name?")
-# lastName = input()
-# print("Nice! " + firstName + " " + lastName + " huh?")
-# print("your initials are " + firstName[0] + "." + lastName[0] + ".")
+print( capitalFirst + " what is your LAST name? ") 
+
+lastName = input() #get user last name
+lNameLength = len(lastName) #get length of last name
+capitalLast = lastName.capitalize() #capitalize first letter
+fullName = (capitalFirst + " " + capitalLast) 
+fullLength = len(lastName) + len(firstName)
+
+print("Nice! " + capitalFirst + " " + capitalLast + " huh?")
+print("your last name is " +str(lNameLength)+ " characters long and your full name is " + str(fullLength) + " characters long")
+print("your initials are " + capitalFirst[0] + "." + capitalLast[0] + ".")
+print("it was nice to meet you " + fullName)
+
 
